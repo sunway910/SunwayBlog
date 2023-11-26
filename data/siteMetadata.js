a34db52e221f3b0274efa7a34220a94f52355281
0utsiderZhong <1092280043@qq.com>
2023-11-26 23:34:35 +0800

    custom blog property

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,17 +1,16 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Sunway Blog',
+  author: 'Sunway',
+  headerTitle: 'SunwayBlog',
+  description: '每临绝境峰回路转',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
+  email: 'shengweizhong910@gmail.com',
   github: 'https://github.com',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',

4d5146c07383f2129d79168c4b624366ac203202
0utsiderZhong <1092280043@qq.com>
2023-12-18 23:54:11 +0800

    add favorite data

--- data/favoritesData.ts
+++ data/favoritesData.ts
@@ -1,128 +1,158 @@
 const favoritesData = [
-  {
-    title: 'Tech (收藏的技术网站)',
-    linkList:[
-        {
-          title:'TheByteBook',
-          description:`深入讲解内核网络、Kubernetes、ServiceMesh、容器等云原生相关技术`,
-          href: 'https://www.thebyte.com.cn/',
-          visit:'Visit TheByteBook'
-        },
-        {
-          title:'Baeldung',
-          description:`专注于Java生态的Baeldung`,
-          href: 'https://www.baeldung.com/',
-          visit:'Visit Baeldung'
-        },
-        {
-          title:'捕蛇者说',
-          description:`Python周刊`,
-          href: 'https://pythonhunter.org/',
-          visit:'Visit PythonHunter'
-        },
-    ],
-    imgSrc: '',
-    extensions: '/favorites/tech',
-  },
-  {
-    title: 'Blog (收藏的技术博客)',
-    linkList:[
-      {
-        title:'plantegg 的博客',
-        description:`plantegg的博客，学习大佬排查问题的思路`,
-        href: 'https://plantegg.github.io/',
-        visit:'Visit Plantegg Blog'
-      },
-      {
-        title:'manjusaka 的博客',
-        description:`SRE大佬的博客`,
-        href: 'https://www.manjusaka.blog',
-        visit:'Visit Manjusaka Blog'
-      },
-      {
-        title:'伪架构师的博客',
-        description:`伪架构师的技术博客，云原生技术实践`,
-        href: 'https://blog.fleeto.us/',
-        visit:'Visit DuSTiSe Blog'
-      },
-      {
-        title:'jokerbai 的博客',
-        description:`jokerbai的运维开发故事`,
-        href: 'https://www.jokerbai.com/',
-        visit:'Visit Jokerbai Blog'
-      },
-      {
-        title:'so1n 的博客',
-        description:`Python实践指引`,
-        href: 'https://so1n.me/',
-        visit:'Visit so1n Blog'
-      },
-    ],
-    imgSrc: '',
-    extensions: '/favorites/blog',
-  },
-  {
-    title: 'Tool (收藏的开发工具)',
-    linkList:[
-      {
-        title:'Helm Chart Template',
-        description:`开源Helm Chart模板`,
-        href: 'https://artifacthub.io/',
-        visit:'Visit ArtifactHub'
-      },
-      {
-        title:'前端ICON资源',
-        description:`前端ICON资源`,
-        href: 'https://www.flaticon.com/packs/essential-collection',
-        visit:'Visit Flaticon'
-      },
-      {
-        title:'TailWindCSS Resource',
-        description:`Tailwind资源库`,
-        href: 'https://github.com/icopy-site/awesome-cn/blob/master/docs/awesome/awesome-tailwindcss.md',
-        visit:'Visit Awesome-Tailwindcss'
-      },
-      {
-        title:'Json2Yaml',
-        description:`json2yaml`,
-        href: 'https://www.json2yaml.com/',
-        visit:'Visit Json2Yaml'
-      },
-      {
-        title:'CloudConvert',
-        description:`文件在线格式转换`,
-        href: 'https://cloudconvert.com/',
-        visit:'Visit CloudConvert'
-      },
-      {
-        title:'PasswordGenerator',
-        description:`密码生成器`,
-        href: 'https://suijimimashengcheng.bmcx.com/',
-        visit:'Visit PasswordGenerator'
-      },
-      {
-        title:'站长工具',
-        description:`站长工具包罗万象`,
-        href: 'https://tool.chinaz.com/',
-        visit:'Visit 站长工具'
-      },
-    ],
-    imgSrc: '',
-    extensions: '/favorites/tool',
-  },
-  {
-    title: 'Fun (收藏的有意思的网站)',
-    linkList:[
-      {
-        title:'云旅游',
-        description:`实时看遍全世界`,
-        href: 'https://www.skylinewebcams.com/',
-        visit:'Visit SkyLineWebCams'
-      },
-    ],
-    imgSrc: '',
-    extensions: '/favorites/fun',
-  },
+    {
+        title: 'Tech (收藏的技术网站)',
+        linkList: [
+            {
+                title: 'TheByteBook',
+                description: `深入讲解内核网络、Kubernetes、ServiceMesh、容器等云原生相关技术`,
+                href: 'https://www.thebyte.com.cn/',
+                visit: 'Visit TheByteBook'
+            },
+            {
+                title: 'Baeldung',
+                description: `专注于Java生态的Baeldung`,
+                href: 'https://www.baeldung.com/',
+                visit: 'Visit Baeldung'
+            },
+            {
+                title: '捕蛇者说',
+                description: `Python周刊`,
+                href: 'https://pythonhunter.org/',
+                visit: 'Visit PythonHunter'
+            },
+            {
+                title: 'Kubernetes 云原生应用架构实战手册',
+                description: `Kubernetes 中文指南/云原生应用架构实战手册`,
+                href: 'https://jimmysong.io/kubernetes-handbook/',
+                visit: 'Visit Kubernetes Handbook'
+            },
+        ],
+        imgSrc: '',
+        extensions: '/favorites/tech',
+    },
+    {
+        title: 'Blog (收藏的技术博客)',
+        linkList: [
+            {
+                title: 'plantegg 的博客',
+                description: `plantegg的博客，学习大佬排查问题的思路`,
+                href: 'https://plantegg.github.io/',
+                visit: 'Visit Plantegg Blog'
+            },
+            {
+                title: 'manjusaka 的博客',
+                description: `SRE大佬的博客`,
+                href: 'https://www.manjusaka.blog',
+                visit: 'Visit Manjusaka Blog'
+            },
+            {
+                title: '伪架构师的博客',
+                description: `伪架构师的技术博客，云原生技术实践`,
+                href: 'https://blog.fleeto.us/',
+                visit: 'Visit DuSTiSe Blog'
+            },
+            {
+                title: 'jokerbai 的博客',
+                description: `jokerbai的运维开发故事`,
+                href: 'https://www.jokerbai.com/',
+                visit: 'Visit Jokerbai Blog'
+            },
+            {
+                title: 'so1n 的博客',
+                description: `Python实践指引`,
+                href: 'https://so1n.me/',
+                visit: 'Visit so1n Blog'
+            },
+        ],
+        imgSrc: '',
+        extensions: '/favorites/blog',
+    },
+    {
+        title: 'Tool (收藏的开发工具)',
+        linkList: [
+            {
+                title: 'Helm Chart Template',
+                description: `Opensource Helm Chart Template`,
+                href: 'https://artifacthub.io/',
+                visit: 'Visit ArtifactHub'
+            },
+            {
+                title: 'HuggingFace',
+                description: `Opensource Model`,
+                href: 'https://huggingface.co/',
+                visit: 'Visit HuggingFace'
+            },
+            {
+                title: 'devv',
+                description: `程序员搜索引擎`,
+                href: 'https://devv.ai/',
+                visit: 'Visit devv'
+            },
+            {
+                title: 'Dev Tutorial',
+                description: `Everything Tutorial`,
+                href: 'https://www.tutorialspoint.com/index.htm',
+                visit: 'Visit TutorialsPoint'
+            },
+            {
+                title: 'Vercel V0',
+                description: `自动生成前端代码`,
+                href: 'https://v0.dev/',
+                visit: 'Visit V0'
+            },
+            {
+                title: '前端ICON资源',
+                description: `前端ICON资源`,
+                href: 'https://www.flaticon.com/packs/essential-collection',
+                visit: 'Visit Flaticon'
+            },
+            {
+                title: 'TailWindCSS Resource',
+                description: `Tailwind资源库`,
+                href: 'https://github.com/icopy-site/awesome-cn/blob/master/docs/awesome/awesome-tailwindcss.md',
+                visit: 'Visit Awesome-Tailwindcss'
+            },
+            {
+                title: 'Json2Yaml',
+                description: `json2yaml`,
+                href: 'https://www.json2yaml.com/',
+                visit: 'Visit Json2Yaml'
+            },
+            {
+                title: 'CloudConvert',
+                description: `文件在线格式转换`,
+                href: 'https://cloudconvert.com/',
+                visit: 'Visit CloudConvert'
+            },
+            {
+                title: 'PasswordGenerator',
+                description: `密码生成器`,
+                href: 'https://suijimimashengcheng.bmcx.com/',
+                visit: 'Visit PasswordGenerator'
+            },
+            {
+                title: '站长工具',
+                description: `站长工具包罗万象`,
+                href: 'https://tool.chinaz.com/',
+                visit: 'Visit 站长工具'
+            },
+        ],
+        imgSrc: '',
+        extensions: '/favorites/tool',
+    },
+    {
+        title: 'Fun (收藏的有意思的网站)',
+        linkList: [
+            {
+                title: '云旅游',
+                description: `实时看遍全世界`,
+                href: 'https://www.skylinewebcams.com/',
+                visit: 'Visit SkyLineWebCams'
+            },
+        ],
+        imgSrc: '',
+        extensions: '/favorites/fun',
+    },
 ]
 
 export default favoritesData

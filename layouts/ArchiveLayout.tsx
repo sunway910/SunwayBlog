910d82854f025cd1f09dac325d93dcdd638a4571
sunway910 <shengweizhong910@gmail.com>
2024-10-06 11:10:03 +0800

    feat: archive blog collapsed by year

--- layouts/ArchiveLayout.tsx
+++ layouts/ArchiveLayout.tsx
@@ -1,8 +1,10 @@
+"use client";
 import { CoreContent } from 'pliny/utils/contentlayer';
 import { Blog } from 'contentlayer/generated';
 import dayjs from 'dayjs';
 import { truncateSummary } from 'app/utils';
 import Link from '@/components/Link';
+import {useState} from "react";
 
 const blogDateTemplate: Intl.DateTimeFormatOptions = {
   weekday: 'long',
@@ -19,13 +21,20 @@ interface YearGroup {
 
 export default function AuthorLayout({ blogs }: Props) {
   const blogsSortedByYear: YearGroup = {};
-  blogs.forEach((blog) => {
+  blogs.reverse().forEach((blog) => {
     const year = dayjs(blog.date).year();
     if (!blogsSortedByYear[year]) {
       blogsSortedByYear[year] = [];
     }
     blogsSortedByYear[year].push(blog);
   });
+  const [collapsedYears, setCollapsedYears] = useState<{ [key: string]: boolean }>({});
+  const toggleCollapse = (year: string) => {
+    setCollapsedYears((prev) => ({
+      ...prev,
+      [year]: !prev[year],
+    }));
+  };
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
@@ -36,32 +45,35 @@ export default function AuthorLayout({ blogs }: Props) {
         </div>
         {
           Object.keys(blogsSortedByYear).map((year) => (
-            // split by year
-            <section key={year}>
-              <div className="sticky top-0 pt-4 bg-white dark:bg-gray-950 shadow-xl shadow-white dark:shadow-gray-950 z-10">
-                <h2 className="text-4xl text-gray-300 px-3 py-4 font-bold tracking-widest">{year}</h2>
-              </div>
-              <div className="border-l-4 border-primary-300 ml-3 py-16 space-y-14">
+              <section key={year}>
+                <div className="sticky top-0 pt-4 bg-white dark:bg-gray-950 shadow-xl shadow-white dark:shadow-gray-950 z-10">
+                  <h1 className="text-4xl text-gray-300 px-3 py-4 font-bold tracking-widest" onClick={() => toggleCollapse(year)}>
+                    {year} {collapsedYears[year] ? '-' : '+'}
+                  </h1>
+                </div>
                 {
-                  blogsSortedByYear[year].map((blog) => (
-                    <div key={blog.slug} className="relative">
-                      <div className="absolute top-1/2 -mt-2.5 -left-3 bg-white dark:bg-black h-5 w-5 rounded-full border-4 border-primary-500"></div>
-                      <div className="pl-10">
-                        <span className="leading-6 text-gray-500 dark:text-gray-400">
-                          {new Date(blog.date).toLocaleDateString('en-us', blogDateTemplate)}
-                        </span>
-                        <h3 className="text-3xl text-gray-600 font-semibold tracking-wide mb-2 mt-1">
-                          <Link href={`/${blog.path}`} className="text-gray-900 dark:text-gray-100 hover:text-primary-500 dark:hover:text-primary-400">
-                            {blog.title}
-                          </Link>
-                        </h3>
-                        <p className="prose max-w-none text-gray-500 dark:text-gray-400">{truncateSummary(blog.summary)}</p>
-                      </div>
+                  !collapsedYears[year] && (
+                    <div>
+                      {
+                        blogsSortedByYear[year].map((blog) => (
+                            <div key={blog.slug} className="relative">
+                              <div className="absolute top-1/2 -mt-2.5 -left-3 bg-white dark:bg-black h-5 w-5 rounded-full border-4 border-primary-500"></div>
+                              <div className="my-10 pl-10">
+                                <h3 className="text-3xl text-gray-600 font-semibold tracking-wide mb-2 mt-1">
+                                  <Link href={`/${blog.path}`} className="text-gray-900 dark:text-gray-100 hover:text-primary-500 dark:hover:text-primary-400">
+                                    {blog.title}
+                                  </Link>
+                                  <p className="prose max-w-none text-gray-500 dark:text-gray-400">{truncateSummary(blog.summary)}</p>
+                                </h3>
+                              </div>
+
+                            </div>
+                        ))
+                      }
                     </div>
-                  ))
+                )
                 }
-              </div>
-            </section>
+              </section>
           ))
         }
       </div>
